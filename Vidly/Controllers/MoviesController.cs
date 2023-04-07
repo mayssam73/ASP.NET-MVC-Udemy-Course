6a5bee89615e7090964134075f78e92ec3ef2fce
Mayssam Kalajo <mayssamkalajo@gmail.com>
2023-04-07 12:52:11 -0500

    Made some comments on MoviesController

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,10 +10,19 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        // Should set this as ViewResult instead of ActionResult, since it's just returning a view
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+            // Can alternatively do
+            // return ViewResult();
+            // Other return types
+            // return Content("Hello World!")
+            // return HttpNotFound()
+            // return new EmptyResult()
+            // For the following, the first parameter is the action, the second is the controller, and the third is the action to redirect to
+            // return RedirectToAction("Index", "Home". new { page = 1, sortBy = "name" });
         }
     }
 }
